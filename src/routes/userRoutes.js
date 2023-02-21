29353fd74954d4d725f7959eb5c25d408b51d1c6
Piotr Gerke <piotrgerke@Piotrs-MBP.localdomain>
2023-02-21 12:02:41 +0100

    redeploy

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -52,7 +52,7 @@ router.patch('/updateUserData/:id', requireAuth, async (req, res) => {
       const user = await User.findById(id);
       
       if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({ message: 'User not found.' });
       }
       
       if (contactEmail) {
