4737b4979388136d41a8150577aad3f30cbf117c
Piotr Gerke <piotrgerke@Piotrs-MBP.localdomain>
2023-02-24 20:09:18 +0100

    invoices for updating plan

--- src/routes/stripeRoutes.js
+++ src/routes/stripeRoutes.js
@@ -9,7 +9,7 @@ const Payment = mongoose.model('Payment');
 const Transaction = mongoose.model('Transaction');
 const axios = require('axios');
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_TEST_SECRET_KEY);
 const purchaseEndpointSecret = 'whsec_NKIX7ahLbstNif0WGd7AIsIy170RqOzc';
 const subscriptionEndpointSecret = 'whsec_NInmuuTZVfBMnfTzNFZJTl0I67u62GCz';
 const infaktConfig = {
@@ -463,7 +463,6 @@ router.post('/update-subscription', requireAuth, async (req, res) => {
       await stripe.subscriptions.del( //delete previous subscription
         subscriptions.data[0].id
       );
-      console.log(subscriptions)
     }
 
     await new Promise(resolve => setTimeout(resolve, 2000)); //wait for stripe
@@ -481,7 +480,113 @@ router.post('/update-subscription', requireAuth, async (req, res) => {
       ],
     });
 
-    res.status(200).json({ message: "Subscription updated" });
+    const plan = await Plan.findById(planId);
+    user.plan = plan;
+    user.tokenBalance += plan.monthlyTokens;
+
+    const purchase = new Payment({
+        price: plan.price,
+        tokens: plan.monthlyTokens,
+        title: `Aktywacja subskrypcji ${plan.name}`,
+        type: "one-time",
+        timestamp: new Date()
+    });
+    user.purchases.push(purchase);
+
+    const transaction = new Transaction({
+        value: plan.monthlyTokens,
+        title: `Aktywacja subskrypcji ${plan.name}`,
+        type: 'income',
+        timestamp: Date.now()
+    });
+    user.transactions.push(transaction);
+    
+    let invoiceData;
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const name = user.fullName;
+    const lastName = name.split(" ")[1];
+    const firstname = name.split(" ")[0];
+    if(!(user.companyName.trim().length === 0)){
+      invoiceData = { //for companies
+        invoice: {
+          "client_company_name": user.companyName, 
+          "invoice_date": `${year}-${month}-${day}`,
+          "sale_date": `${year}-${month}-${day}`,
+          "payment_method": "card",
+          "status": "paid",
+          "paid_date": `${year}-${month}-${day}`,
+          "client_first_name": firstname,
+          "client_last_name": lastName,
+          "client_street": user.street,
+          "client_street_number": user.apartmentNumber,
+          "client_city": user.city,
+          "client_post_code": user.postalCode,
+          "client_tax_code": user.nip,
+          "client_country": "Polska",
+          "paid_price": plan.price * 100, 
+          "services":[
+            {
+               "name": `Miesięczna Subskrypcja Oprogramowania Aplikacji AsystentAI (Pakiet ${plan.name})`, 
+               "pkwiu": "62.01", 
+               "tax_symbol": 23,
+               "gross_price": plan.price * 100, 
+            }
+          ]
+        },
+      };
+    } else { //for individuals
+      invoiceData = {
+        invoice: {
+          "client_company_name": user.fullName, 
+          "invoice_date": `${year}-${month}-${day}`,
+          "sale_date": `${year}-${month}-${day}`,
+          "status": "paid",
+          "paid_date": `${year}-${month}-${day}`,
+          "payment_method": "card",
+          "client_first_name": firstname,
+          "client_last_name": lastName,
+          "client_street": user.street,
+          "client_street_number": user.apartmentNumber,
+          "client_city": user.city,
+          "client_post_code": user.postalCode,
+          "client_country": "Polska",
+          "paid_price": plan.price * 100, 
+          "client_tax_code": "",
+          "services":[
+            {
+               "name": `Miesięczna Subskrypcja Oprogramowania Aplikacji AsystentAI (Pakiet ${plan.name})`,
+               "pkwiu": "62.01", 
+               "tax_symbol": 23,
+               "gross_price": plan.price * 100, 
+            }
+          ]
+        },
+      };
+    }
+    const balanceSnapshot = {
+      timestamp: Date.now(),
+      balance: user.tokenBalance
+    };
+    user.tokenHistory.push(balanceSnapshot);
+
+    try {
+      await axios.post('https://api.infakt.pl/v3/invoices.json', invoiceData, infaktConfig);
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      await user.save();
+      await transaction.save();
+      await purchase.save();
+      const latestInvoice = await axios.get('https://api.infakt.pl/v3/invoices.json?limit=1', infaktConfig);
+      const lastInvoiceID = latestInvoice.data.entities[0].id;
+      await axios.post(`https://api.infakt.pl/v3/invoices/${lastInvoiceID}/paid.json`, {invoice: {"status": "paid"}}, infaktConfig);
+      await axios.post(`https://api.infakt.pl/v3/invoices/${lastInvoiceID}/deliver_via_email.json`, {"print_type": "original"}, infaktConfig);
+    } catch (error) {
+      console.error(`Error: ${JSON.stringify(error.response.data)}`);
+    }
+    
+    res.status(200).json({ message: "Subscription updated", subscription });
 } catch (e) {
   res.status(500).json({message: "Error updating subscripiton", error: e})
 }
