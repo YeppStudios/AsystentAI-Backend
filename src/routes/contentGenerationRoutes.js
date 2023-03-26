659f11ca0805d9ef8c9b954f81e000c4b59d8f79
Piotr Gerke <piotrgerke@Piotrs-MBP.localdomain>
2023-03-26 20:26:31 +0200

    update

--- src/routes/contentGenerationRoutes.js
+++ src/routes/contentGenerationRoutes.js
@@ -136,9 +136,11 @@ router.post('/messageAI', requireTokens, async (req, res) => {
         let inputTokens = 0;
         let outputTokens = 0;
         let reply = '';
-        conversationContext.forEach(message => {
-            inputTokens += estimateTokens(message.content);
-        });
+        if(conversationContext.lenght > 0){
+            conversationContext.forEach(message => {
+                inputTokens += estimateTokens(message.content);
+            });
+        }
         const completion = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: conversationContext,
