9d900e22edf2ea36d9fcb816b23cf31008afd3d0
Piotr Gerke <piotrgerke@piotrs-macbook-pro.home>
2023-04-08 11:13:03 +0200

    text editor endpoint

--- src/routes/contentGenerationRoutes.js
+++ src/routes/contentGenerationRoutes.js
@@ -229,6 +229,108 @@ router.post('/messageAI', requireTokens, async (req, res) => {
     }
 });
 
+router.post('/compose-editor-completion', requireTokens, async (req, res) => {
+  try {
+      const { prompt, model } = req.body;
+      const user = req.user;
+      let inputTokens = 0;
+      let outputTokens = 0;
+      let reply = '';
+      let messages = [
+          { role: 'system', content: 'Jesteś przyjaznym, pomocnym i wszechwiedzącym copywriterem, który nazywa się Asystent AI. Jesteś mistrzem w generowaniu wysokiej jakości treści marketingowych i SEO. Zawsze zamiast zaczynać od nowej linii wstawiasz spacje.' },
+          { role: 'user', content: prompt }
+      ]
+      messages.forEach(message => {
+          inputTokens += estimateTokens(message.content);
+      });
+      const completion = await openai.createChatCompletion({
+          model: model,
+          messages,
+          temperature: 0.7,
+          frequency_penalty: 0.35,
+          stream: true,
+      }, { responseType: 'stream' });
+      res.set({
+          'Content-Type': 'text/event-stream',
+          'Cache-Control': 'no-cache',
+          'Connection': 'keep-alive'
+      });
+
+      const heartbeatInterval = setInterval(() => {
+          res.write(':heartbeat\n\n');
+      }, 28000);
+
+      completion.data.on('data', async data => {
+          const lines = data.toString().split('\n').filter(line => line.trim() !== '');
+          for (const line of lines) {
+            const message = line.replace(/^data: /, '');
+            if (message === '[DONE]') {
+              res.write('\n\n');
+              res.end();
+              return;
+            } else {
+              try {
+                const parsed = JSON.parse(message);
+                if(parsed.choices[0].finish_reason === "stop"){
+                  res.write('\n\n');
+                  outputTokens = estimateTokens(reply);
+                  const totalTokens = inputTokens + outputTokens;
+                  user.tokenBalance -= (totalTokens);
+
+                  const transaction = new Transaction({
+                      value: totalTokens,
+                      title: "text editor completion",
+                      type: "expense",
+                      timestamp: Date.now(),
+                      category: "text-editor",
+                      user: user._id
+                  });
+          
+                  user.transactions.push(transaction);
+                  
+                  user.tokenHistory.push({
+                      timestamp: Date.now(),
+                      balance: user.tokenBalance
+                  });
+          
+                  await user.save();
+                  await transaction.save();
+                  res.end();
+                  return;
+                } else if(parsed.choices[0].delta.content) {
+                  res.write(`data: ${JSON.stringify(parsed.choices[0].delta)}\n\n`);
+                  reply += parsed.choices[0].delta.content;
+                }
+              } catch(error) {
+                console.error('Could not JSON parse stream message', message, error);
+              }
+            }
+          }
+        });
+
+        req.on('close', () => {
+          clearInterval(heartbeatInterval);
+        });
+
+  } catch (error) {
+      if (error.response?.status) {
+          console.error(error.response.status, error.message);
+          error.response.data.on('data', data => {
+              const message = data.toString();
+              try {
+                  const parsed = JSON.parse(message);
+                  console.error('An error occurred during OpenAI request: ', parsed);
+              } catch(error) {
+                  console.error('An error occurred during OpenAI request: ', message);
+              }
+          });
+      } else {
+          console.error('An error occurred during OpenAI request', error);
+      }
+  }
+});
+
+
 router.post('/testMessageAI', requireTestTokens, async (req, res) => {
     try {
         const { conversationContext } = req.body;
