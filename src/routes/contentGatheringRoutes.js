abec22c3582c40c60fe3fbfcf9725380d02d20bd
Piotr Gerke <piotrg2003@gmail.com>
2023-04-12 17:04:58 +0200

    get content endpoint

--- src/routes/contentGatheringRoutes.js
+++ src/routes/contentGatheringRoutes.js
@@ -72,6 +72,19 @@ router.get('/getSavedContent', requireAdmin, async (req, res) => {
       res.status(400).json({ message: error.message });
     }
   });
+
+  router.get('/getContentPiece/:id', requireAuth, async (req, res) => {
+    try {
+      const content = await Content.findById(req.params.id);
+      if (!content) {
+        res.status(404).json({ message: 'Content not found' });
+      } else {
+        res.status(200).json(content);
+      }
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
   
   router.get('/getUserSavedContent', requireAuth, async (req, res) => {
     try {
