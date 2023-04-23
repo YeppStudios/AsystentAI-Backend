3fd70e5520e2494095d901bffe41c6e0273f5e17
Piotr Gerke <piotrg2003@gmail.com>
2023-04-23 14:32:20 +0200

    error- reservation for this email already exists

--- src/routes/conferenceRoutes.js
+++ src/routes/conferenceRoutes.js
@@ -3,16 +3,23 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Reservation = mongoose.model('Reservation');
 
-// POST /reservations
-router.post('/addReservation', async (req, res) => {
-  try {
-    const { fullName, spots, companyName, email, phone } = req.body;
-    const reservation = new Reservation({ fullName, spots, companyName, email, phone });
-    await reservation.save();
-    res.status(201).json(reservation);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
 
+router.post('/reservations', async (req, res) => {
+    try {
+      const { fullName, spots, companyName, email, phone } = req.body;
+  
+      // Check if there is already a reservation with this email
+      const existingReservation = await Reservation.findOne({ email });
+      if (existingReservation) {
+        return res.status(400).json({ error: 'There is already a reservation with this email address' });
+      }
+  
+      const reservation = new Reservation({ fullName, spots, companyName, email, phone });
+      await reservation.save();
+      res.status(201).json(reservation);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+  
 module.exports = router;
