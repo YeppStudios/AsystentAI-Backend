f0a7011b49c33bd6a0e218e236533f9fe24f43c4
Gerke <piotrg2003@gmail.com>
2023-07-10 16:56:48 +0200

    mailchimp

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -150,13 +150,13 @@ router.post('/register-free-trial', async (req, res) => {
       });
 
       try {
-        // await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
-        //   email_address: email,
-        //   status: 'subscribed',
-        //   merge_fields: {
-        //     FNAME: name,
-        //   },
-        // });
+        await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
+          email_address: email,
+          status: 'subscribed',
+          merge_fields: {
+            FNAME: name,
+          },
+        });
       } catch (error) {
         console.error('Failed to add user to Mailchimp audience:', error.message);
       }
