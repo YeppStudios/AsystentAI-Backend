5e2395a19573c1dd93c9caa2e718fec58a1ea6b2
Gerke <piotrg2003@gmail.com>
2023-08-01 12:24:05 +0200

    updated pricing updates

--- src/routes/stripeRoutes.js
+++ src/routes/stripeRoutes.js
@@ -412,11 +412,13 @@ router.post('/subscription-checkout-webhook', bodyParser.raw({type: 'application
             planId = "64ad0d250e40385f299bceea"
           }  else if (priceId === "price_1NSZjsFe80Kn2YGGYa3pzseT") { //Standard mo
             planId = "64ad0d740e40385f299bcef9"
-          }  else if (priceId === "price_1NSafTFe80Kn2YGG5LVrITu1") { //Standard 3mo
+          } else if (priceId === "price_1NaF8EFe80Kn2YGGAuVBGHjh") { //Standard mo
             planId = "64ad0d740e40385f299bcef9"
-          }  else if (priceId === "price_1NSagKFe80Kn2YGGFhOuum7Z") { //Standard 6mo
+          } else if (priceId === "price_1NaFKUFYGGN8gOfDnT") { //Standard 3mo
+            planId = "64ad0d740e40385f299be80Kn2cef9"
+          }  else if (priceId === "price_1NaFLfFe80Kn2YGGFtgjV1CI") { //Standard 6mo
             planId = "64ad0d740e40385f299bcef9"
-          }  else if (priceId === "price_1NSaglFe80Kn2YGGZZ8msY1r") { //Standard 12mo
+          }  else if (priceId === "price_1NaFMhFe80Kn2YGGsXAeqFPF") { //Standard 12mo
             planId = "64ad0d740e40385f299bcef9"
           } else if (priceId === "price_1NVVfpFe80Kn2YGGDCHIg1aX") { ///Assistant Business Monthly discount
             planId = "6444d4394ab2cf9819e5b5f4";
