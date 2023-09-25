c69775320761f2cb54c9a45234b4e080c5357234
Gerke <piotrg2003@gmail.com>
2023-09-25 15:23:42 +0200

    updated partnership

--- src/models/WithdrawalRequest.js
+++ src/models/WithdrawalRequest.js
@@ -5,35 +5,9 @@ const WithdrawalRequestSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, 
         ref: 'User' 
     },
-    companyName: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    fullName: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    city: {
-        type: String,
-        required: true,
-    },
-    country: {
-        type: String,
-        required: true,
-    },
     ibanNumber: {
         type: String,
-        required: true,
-        unique: true,
+        required: true
     },
 });
 mongoose.model('WithdrawalRequest', WithdrawalRequestSchema);
