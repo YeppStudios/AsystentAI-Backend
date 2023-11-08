b020345738ffb73b0999ec6b26676f091ad87d71
Gerke <piotrg2003@gmail.com>
2023-11-07 16:20:41 -0800

    test endpoint

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -50,6 +50,19 @@ router.get('/users/:id', requireAuth, async (req, res) => {
     }
 });
 
+// Get a single user
+router.get('/users_test/:id', async (req, res) => {
+  try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+      }
+      return res.status(200).json(user);
+  } catch (error) {
+      return res.status(500).json({ message: error.message });
+  }
+});
+
 // Update a user
 router.patch('/updateUser/:id', requireAdmin, async (req, res) => {
     try {
